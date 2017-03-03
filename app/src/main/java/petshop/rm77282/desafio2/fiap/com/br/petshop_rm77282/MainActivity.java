8086fb61ac8989b3513cf58c40737a26520861aa
Fernando Mazola <fernando_mazola@hotmail.com>
2017-03-03 08:44:06 -0300

    First Commit e Adicionado Arquivo Gitignore

---
+++ app/src/main/java/petshop/rm77282/desafio2/fiap/com/br/petshop_rm77282/MainActivity.java
@@ -0,0 +1,13 @@
+package petshop.rm77282.desafio2.fiap.com.br.petshop_rm77282;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
